c1e65669931096b5b98950fef71ce5396f7d8949
Zeldak7 <jungmannjannik@gmail.com>
2024-03-27 22:41:18 +0100

    Initial commit

---
+++ src/main/java/net/zeldak7/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package net.zeldak7.myfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
